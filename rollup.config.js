49918d82e9fc02a4c5dbbdcdee338ba5e7c39bc0
Rob Hicks <rob@hixfamily.org>
2017-09-04 08:35:25 -0600

    upgrade rollup to latest and create iife

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,37 @@
-module.exports = {
-	entry: 'src/I18n.js',
-	plugins: [
-		require('rollup-plugin-buble')(),
-	],
-	external: [],
-	targets: [
-		{
-			dest: 'dist/I18n.cjs.js',
-			format: 'cjs',
-		},
-		{
-			dest: 'dist/I18n.es.js',
-			format: 'es',
-		}
-	]
-};
+const buble = require('rollup-plugin-buble');
+let input = 'src/I18n.js';
+let plugins = [
+  buble()
+];
+let globals = [];
+
+export default [
+  {
+    input,
+    plugins,
+    globals,
+    name: 'I18n',
+    output: {
+      file: 'dist/I18n.iife.js',
+      format: 'iife'
+    }
+  },
+  {
+    input,
+    plugins,
+    globals,
+    output: {
+      file: 'dist/I18n.cjs.js',
+      format: 'cjs'
+    }
+  },
+  {
+    input,
+    plugins,
+    globals,
+    output: {
+      file: 'dist/I18n.es.js',
+      format: 'es'
+    }
+  }
+];
