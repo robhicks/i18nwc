885e42237c6bbd5c4efe204f09fb8fdc83078bd7
Rob Hicks <rob@hixfamily.org>
2021-10-16 18:28:56 -0700

    update deps to remove Depndabot alerts

--- dist/I18nwc.es.js
+++ dist/I18nwc.es.js
@@ -5,28 +5,28 @@ class I18n {
     this.splitter = options.splitter || '::';
 
     return (...key) => {
-      let replacements, count;
+      let replacements; let count;
 
       if (key[1] && typeof key[1] === 'object') replacements = key[1];
       else if (key[2] && typeof key[2] === 'object') replacements = key[2];
 
       if (Number.isInteger(key[1])) count = key[1];
-      else if(Number.isInteger(key[2])) count = key[2];
+      else if (Number.isInteger(key[2])) count = key[2];
 
       let translation = this.getTranslation(key[0]);
 
       if (count && replacements) {
         replacements.n = replacements.n ? replacements.n : count;
 
-        //get appropriate plural translation string
+        // get appropriate plural translation string
         translation = this.getPlural(translation, count);
       }
 
-      //replace {placeholders}
+      // replace {placeholders}
       if (replacements) translation = this.replacePlaceholders(translation, replacements);
 
       if (!translation === null) {
-        console.warn('Translation for "' + key + '" not found.');
+        console.warn(`Translation for "${key}" not found.`);
       }
 
       return translation;
@@ -37,42 +37,40 @@ class I18n {
     if (!key) return null;
     if (this.message.hasOwnProperty(key)) return this.message[key];
     try {
-      let components = key.split(this.splitter);
-      let namespace = components[0];
-      let _key = components[1];
+      const components = key.split(this.splitter);
+      const namespace = components[0];
+      const _key = components[1];
       if (this.message.hasOwnProperty(namespace) && this.message[namespace][_key]) return this.message[namespace][_key];
       return key;
-    } catch(e) {
+    } catch (e) {
       return key;
     }
   }
 
   getPlural(translation, count) {
-    let i, _translation, upper;
+    let i; let _translation; let upper;
     if (typeof translation === 'object') {
-      let keys = Object.keys(translation);
+      const keys = Object.keys(translation);
       if (keys.length === 0) return null;
       for (i = 0; i < keys.length; i++) {
         if (keys[i].indexOf('gt' === 0)) upper = parseInt(keys[i].replace('gt', ''), 10);
       }
       if (translation[count]) _translation = translation[count];
-      else if (count > upper) _translation = translation['gt' + upper];
+      else if (count > upper) _translation = translation[`gt${upper}`];
       else if (translation.n) _translation = translation.n;
       else _translation = translation[Object.keys(translation).reverse()[0]];
 
       return _translation;
     }
-
   }
 
   replacePlaceholders(translation, replacements) {
-    let t = typeof translation === 'string'
+    const t = typeof translation === 'string'
       ? translation.replace(/\{(\w*)\}/g, (match, key) => replacements[key])
       : translation;
 
     return t;
   }
-
 }
 
-export default I18n;
+export { I18n as default };
